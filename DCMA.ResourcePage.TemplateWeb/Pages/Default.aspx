3c24c0e9806e0ff27ffa71097ef4302805566aa7
Tesfaye.Wojago <Tesfaye.Wojago@NESassociayes.com>
2017-05-08 11:21:53 -0400

    Add project files.

---
+++ DCMA.ResourcePage.TemplateWeb/Pages/Default.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="DCMA.ResourcePage.TemplateWeb.Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+        <div id="divSPChrome"></div>
+    </div>
+     
+          <div style="left: 40px; position: absolute;">
+            <h2>Create a new policy Resource Page</h2>
+            <br />
+       
+            <br />Resource page name:
+              <asp:TextBox ID="TxtBoxPageName" runat="server"></asp:TextBox>        
+            <asp:Button runat="server" ID="btnCreateResourcePage" Text="Click here to create resource page" OnClick="btnCreateResourcePage_Click" />
+              <br/>
+              
+        <img src="images/OneColumn.PNG" /> 
+              <br/>
+                 <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ DCMA.ResourcePage.TemplateWeb/Pages/Default.aspx.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DCMA.ResourcePage.TemplateWeb
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            Uri redirectUrl;
+            switch (SharePointContextProvider.CheckRedirectionStatus(Context, out redirectUrl))
+            {
+                case RedirectionStatus.Ok:
+                    return;
+                case RedirectionStatus.ShouldRedirect:
+                    Response.Redirect(redirectUrl.AbsoluteUri, endResponse: true);
+                    break;
+                case RedirectionStatus.CanNotRedirect:
+                    Response.Write("An error occurred while processing your request.");
+                    Response.End();
+                    break;
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string script = @"
+            function chromeLoaded() {
+                $('body').show();
+            }
+
+            //function callback to render chrome after SP.UI.Controls.js loads
+            function renderSPChrome() {
+                //Set the chrome options for launching Help, Account, and Contact pages
+                var options = {
+                    'appTitle': document.title,
+                    'onCssLoaded': 'chromeLoaded()'
+                };
+
+                //Load the Chrome Control in the divSPChrome element of the page
+                var chromeNavigation = new SP.UI.Controls.Navigation('divSPChrome', options);
+                chromeNavigation.setVisible(true);
+            }";
+
+            //register script in page which shows the content when chrome was loaded
+            Page.ClientScript.RegisterClientScriptBlock(typeof(Default), "BasePageScript", script, true);
+        }
+
+        protected void btnCreateResourcePage_Click(object sender, EventArgs e)
+        {
+            var spContext = SharePointContextProvider.Current.GetSharePointContext(Context);
+            using (var clctx = spContext.CreateUserClientContextForSPHost())
+            {
+
+                var PagesUrl = clctx.Web.Lists.GetByTitle("Site Pages");
+                clctx.Load(PagesUrl.RootFolder, f => f.ServerRelativeUrl);
+                clctx.Load(PagesUrl.RootFolder.Files);
+                clctx.ExecuteQuery();
+                string strUrl = string.Format("{0}/{1}", PagesUrl.RootFolder.ServerRelativeUrl, "Template" + ".aspx");
+                File templateFile = clctx.Web.Lists.GetByTitle("Site Pages").RootFolder.Files.GetByUrl(strUrl);
+                clctx.Load(templateFile, b => b.Exists);
+                clctx.ExecuteQuery();
+                try
+                {
+                    templateFile.CopyTo(PagesUrl.RootFolder.ServerRelativeUrl + "/" + TxtBoxPageName.Text + ".aspx", false);
+                    clctx.ExecuteQuery();
+                    Label1.Text = "<b>Page is created successfully<b>";
+                }
+                catch (Exception)
+                {
+
+                    Label1.Text = "<b>Page is not created with name provided, please try another name or contact system administrator if problem persists.<b>";
+                }
+
+
+            }
+
+
+        }
+    }
+}
